41d914ff8da9665f8832040d597c29f3a44d879e
Muhammad Murtaza <muhammad.murtaazaa@gmail.com>
2025-07-12 15:41:13 +0500

    updated server

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ app.use('/api/water', require('./routes/water'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('Welcome to the WellnessApp API!');
+});
+
